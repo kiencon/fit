e2478d977e6afdd49d55dec2179adf1a7f646b4c
kiencon <vietson1610@gmail.com>
2017-09-06 14:46:00 +0700

    new file

---
+++ fit/js/js1.js
@@ -0,0 +1,49 @@
+jQuery(document).ready(function($) {
+"use strict";
+$('#customers-feeling').owlCarousel( {
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 30,
+		autoplay: true,
+		dots:true,
+    nav:true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+  	navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 1
+			}
+		}
+	});
+$('#customers-comment').owlCarousel( {
+		loop: true,
+		center: true,
+		items: 3,
+		margin: 30,
+		autoplay: true,
+		dots:true,
+    nav:true,
+		autoplayTimeout: 8500,
+		smartSpeed: 450,
+  	navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 1
+			},
+			1170: {
+				items: 3
+			}
+		}
+	});
+});
